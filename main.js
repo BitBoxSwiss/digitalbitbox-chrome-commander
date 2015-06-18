79afedbd641b8b715c05a7f6f2af89134888ba33
djb <digitalbitbox@gmail.com>
2015-06-19 00:57:25 +0200

    adjust response window for ubuntu

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create(
       "control-panel.html",
       {
-        innerBounds: { width: 940, height: 440, minWidth: 940 }
+        innerBounds: { width: 840, height: 440, minWidth: 840 }
       });
 });
 
